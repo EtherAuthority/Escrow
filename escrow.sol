a24c53db39c45c6eef1a7c0336033c8e0f20b7dd
Diptibaral1993 <33413243+Diptibaral1993@users.noreply.github.com>
2020-03-07 20:17:27 +0530

    Update escrow.sol

--- escrow.sol
+++ escrow.sol
@@ -1,6 +1,57 @@
 pragma solidity ^0.5.16;
 
-contract Escrow {
+
+interface USDT{
+    function transfer(address _to, uint256 _value) external returns(bool);
+    function transferFrom(address _from, address _to, uint256 _value) external returns(bool);
+}
+
+contract owned {
+    address public owner;
+    address internal newOwner;
+
+    /**
+        Signer is deligated admin wallet, which can do sub-owner functions.  Signer calls following  function:
+            => MintTokens
+    */
+    address public signer;
+
+    event OwnershipTransferred(address indexed _from, address indexed _to);
+
+    constructor() public {
+        owner = msg.sender;
+        signer = msg.sender;
+    }
+
+    modifier onlyOwner {
+        require(msg.sender == owner, 'caller must be owner');
+        _;
+    }
+
+    modifier onlySigner {
+        require(msg.sender == signer, 'caller must be signer');
+        _;
+    }
+
+    function changeSigner(address _signer) public onlyOwner {
+        signer = _signer;
+    }
+
+    function transferOwnership(address _newOwner) public onlyOwner {
+        newOwner = _newOwner;
+    }
+
+    //this flow is to prevent transferring ownership to wrong wallet by mistake
+    function acceptOwnership() public {
+        require(msg.sender == newOwner, 'Invalid owner address');
+        emit OwnershipTransferred(owner, newOwner);
+        owner = newOwner;
+        newOwner = address(0);
+    }
+}
+
+
+contract Escrow is owned{
     
     mapping (address => uint256) public buyerBalance;
     //uint balance;
@@ -13,30 +64,42 @@ contract Escrow {
     mapping(address => bool) public _cbuyer;
     mapping(address => bool) public _cseller;
     
-    mapping (address => uint256) public balanceOf;
-    
     address payable escrow;
     uint private start;
+    
+    
+    struct escrowDetail {
+        address buyer;
+        address seller;
+        uint256 amount;
+        bool status;
+    }
+    
+    mapping (uint256 => escrowDetail) public escrowMap;
+    
+    event startEscrow(address _buyer,address _seller, uint256 _amount);
+    
+    event releaseEscrow(address _buyer,address _seller, uint256 _amount);
+    
+    address public usdtadd;
+    address public usdtHolder;
+    
+    
     constructor() public {
         escrow = msg.sender;
         start = now; //now is an alias for block.timestamp, not really "now"
     }
     
-    function startEscrowSigner(address _buyer, address _seller, uint256 _amount) public returns(bool){
+    function startTrade(address _buyer, address _seller) public{
         
         require(Deal[_buyer]!=_seller,"Pending");
-        
-        _amount=_amount*(10**18);
-        buyerBalance[_buyer] += _amount;
-        
         Deal[_buyer]=_seller;
-        return true;
+        
     }
-    
-    
    
+    function () payable external{}
     
-    function releaseEscrowSigner(address payable _buyer, address payable _seller) public returns(bool){
+    function accept(address _buyer, address payable _seller) public {
         
         require(Deal[_buyer]==_seller,"Deal Not Exist");
         if (msg.sender == _buyer){
@@ -46,19 +109,17 @@ contract Escrow {
         }
         if (buyer[_buyer] && seller[_seller]){
             payBalance(_buyer,_seller);
-        } 
-        return true;
+        } else if (buyer[_buyer] && !seller[_seller] && now > start + 30 days) {
+            // Freeze 30 days before release to buyer. The customer has to remember to call this method after freeze period.
+            //selfdestruct(_buyer);
+        }
     }
     
-    
-    function () payable external{}
-    
-    function payBalance(address _buyer, address payable _seller) public payable{
+    function payBalance(address _buyer, address payable _seller) private {
         
         require(Deal[_buyer]==_seller,"Not Possible");
         // we are sending ourselves (contract creator) a fee
         escrow.transfer(address(this).balance / 100);
-        
         // send seller the balance
         if (_seller.send(address(this).balance)) {
             buyerBalance[_buyer] = 0;
@@ -85,25 +146,55 @@ contract Escrow {
         }
         // if both buyer and seller would like to cancel, money is returned to buyer 
         if (_cbuyer[_buyer] && _cseller[_seller]){
-            
-            if (_buyer.send(buyerBalance[_buyer])) {
-                buyerBalance[_buyer] = 0;
-            }
+            _buyer.transfer(address(this).balance);
         }
     }
     
-    function collectUsdt() public{
+     
+    
+    function addUsdt(address _usdtadd,address _usdtOwner) public{
+        usdtadd=_usdtadd;
+        usdtHolder=_usdtOwner;
+    }
+    
+    function stratEscrowSigner(address _buyer,address _seller, uint256 _amount, uint256 escrowID) public{
+        
+        escrowDetail memory startStruct;
+        
+        startStruct=escrowDetail({
+            buyer:_buyer,
+            seller:_seller,
+            amount:_amount,
+            status:true
+            
+        });
+        
+        escrowMap[escrowID]=startStruct;
+        
+        USDT(usdtadd).transferFrom(usdtHolder,address(this),_amount);
+        emit startEscrow(_buyer,_seller,_amount);
         
     }
     
-    function kill(address payable _buyer) public  {
+    
+    function releaseEscrowSigner(address _buyer,address _seller, uint256 _amount, uint256 escrowID) public{
+        escrowDetail memory startStruct;
         
-        require(Deal[_buyer]!=address(0),"Address Doesnot Exist");
-        if (msg.sender == escrow) {
+        startStruct=escrowDetail({
+            buyer:_buyer,
+            seller:_seller,
+            amount:_amount,
+            status:false
             
-            if (_buyer.send(buyerBalance[_buyer])) {
-                buyerBalance[_buyer] = 0;
-            }
-        }
+        });
+        
+        escrowMap[escrowID]=startStruct;
+        
+        USDT(usdtadd).transfer(usdtHolder,_amount);
+        emit releaseEscrow(_buyer,_seller,_amount);
     }
+    
+    
+    
+    
 }
