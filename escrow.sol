b5cbfd3254efce244e75bd2876af7cfa4c2b07bf
Ether Authority <43878603+EtherAuthority@users.noreply.github.com>
2020-03-07 11:30:07 +0530

    dipti changes

--- escrow.sol
+++ escrow.sol
@@ -1,65 +1,93 @@
 pragma solidity ^0.4.11;contract Escrow {
-    uint balance;
-    address public buyer;
-    address public seller;
+    
+    mapping (address => uint256) public buyerBalance;
+    //uint balance;
+    
+    mapping(address => address) public Deal;
+    
+    mapping(address => bool) public buyer;
+    mapping(address => bool) public seller;
+    
+    mapping(address => bool) public _cbuyer;
+    mapping(address => bool) public _cseller;
+    
     address private escrow;
     uint private start;
-    bool buyerOk;
-    bool sellerOk;
-    constructor(address buyer_address, address seller_address) public {
-        // this is the constructor function that runs ONCE upon initialization
-        buyer = buyer_address;
-        seller = seller_address;
+    constructor() public {
         escrow = msg.sender;
         start = now; //now is an alias for block.timestamp, not really "now"
     }
     
-    function accept() public {
-        if (msg.sender == buyer){
-            buyerOk = true;
-        } else if (msg.sender == seller){
-            sellerOk = true;
+    function startTrade(address _buyer, address _seller) public{
+        
+        require(Deal[_buyer]!=_seller,"Pending");
+        Deal[_buyer]=_seller;
+    }
+   
+    
+    function accept(address _buyer, address _seller) public {
+        
+        require(Deal[_buyer]==_seller,"Deal Not Exist");
+        if (msg.sender == _buyer){
+            buyer[_buyer] = true;
+        } else if (msg.sender == _seller){
+            seller[_seller] = true;
         }
-        if (buyerOk && sellerOk){
-            payBalance();
-        } else if (buyerOk && !sellerOk && now > start + 30 days) {
+        if (buyer[_buyer] && seller[_seller]){
+            payBalance(_buyer,_seller);
+        } else if (buyer[_buyer] && !seller[_seller] && now > start + 30 days) {
             // Freeze 30 days before release to buyer. The customer has to remember to call this method after freeze period.
-            selfdestruct(buyer);
+            selfdestruct(_buyer);
         }
     }
     
-    function payBalance() private {
+    function payBalance(address _buyer, address _seller) private {
+        
+        require(Deal[_buyer]==_seller,"Not Possible");
         // we are sending ourselves (contract creator) a fee
         escrow.transfer(address(this).balance / 100);
         // send seller the balance
-        if (seller.send(address(this).balance)) {
-            balance = 0;
+        if (_seller.send(address(this).balance)) {
+            buyerBalance[_buyer] = 0;
         } else {
             revert();
         }
     }
     
-    function deposit() public payable {
-        if (msg.sender == buyer) {
-            balance += msg.value;
+    // function deposit(address _buyer) public payable {
+        
+    //     require(Deal[_buyer]!=address(0),"Address Doesnot Exist");
+    //     if (msg.sender == _buyer) {
+    //         buyerBalance[_buyer] += msg.value;
+    //     }
+    // }
+    
+    function deposit(address _buyer) public payable {
+        if (msg.sender == _buyer) {
+            buyerBalance[_buyer] += msg.value;
         }
     }
     
-    function cancel() public {
-        if (msg.sender == buyer){
-            buyerOk = false;
-        } else if (msg.sender == seller){
-            sellerOk = false;
+    function cancel(address _buyer, address _seller) public {
+        
+        require(Deal[_buyer]==_seller,"Deal Not Exist");
+         
+        if (msg.sender == _buyer){
+            _cbuyer[_buyer] = true;
+        } else if (msg.sender == _seller){
+            _cseller[_seller] = true;
         }
         // if both buyer and seller would like to cancel, money is returned to buyer 
-        if (!buyerOk && !sellerOk){
-            selfdestruct(buyer);
+        if (_cbuyer[_buyer] && _cseller[_seller]){
+            selfdestruct(_buyer);
         }
     }
     
-    function kill() public constant {
+    function kill(address _buyer) public  {
+        
+        require(Deal[_buyer]!=address(0),"Address Doesnot Exist");
         if (msg.sender == escrow) {
-            //selfdestruct(buyer);
+            selfdestruct(_buyer);
         }
     }
 }
